3148b95846a5c55bdf72c8974c78592ac734d28d
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-10-23 13:49:26 +0300

    Create React Root

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // React +18
+root.render(<h1>Hello World 2</h1>);
+
+//ReactDOM.render("Hellow World", document.getElementById("root"));
