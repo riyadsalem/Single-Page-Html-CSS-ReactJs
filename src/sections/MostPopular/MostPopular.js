471be7c5fb0d555d8ac591cf8c43224f76d4dafd
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-10-31 14:17:46 +0200

    Customize MostPopular Component >> Css

--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -32,6 +32,75 @@ const MostPopular = () => {
             {/* End: card-wrapper */}
           </div>
           {/* End: most-popular-item */}
+
+          <div className="most-popular-item">
+            <div className="card-wrapper">
+              <img className="most-popular-image" src={popular_01} />
+              <div className="most-popular-item-content">
+                <h4 className="most-popular-item-title">
+                  Fortnite <br />
+                  <span>Sandbox</span>
+                </h4>
+                <ul>
+                  <li>
+                    <span>4.8</span>
+                  </li>
+                  <li>
+                    <span>2.3</span>
+                  </li>
+                </ul>
+              </div>
+              {/* End:  most-popular-item-content*/}
+            </div>
+            {/* End: card-wrapper */}
+          </div>
+          {/* End: most-popular-item */}
+
+          <div className="most-popular-item">
+            <div className="card-wrapper">
+              <img className="most-popular-image" src={popular_01} />
+              <div className="most-popular-item-content">
+                <h4 className="most-popular-item-title">
+                  Fortnite <br />
+                  <span>Sandbox</span>
+                </h4>
+                <ul>
+                  <li>
+                    <span>4.8</span>
+                  </li>
+                  <li>
+                    <span>2.3</span>
+                  </li>
+                </ul>
+              </div>
+              {/* End:  most-popular-item-content*/}
+            </div>
+            {/* End: card-wrapper */}
+          </div>
+          {/* End: most-popular-item */}
+
+          <div className="most-popular-item">
+            <div className="card-wrapper">
+              <img className="most-popular-image" src={popular_01} />
+              <div className="most-popular-item-content">
+                <h4 className="most-popular-item-title">
+                  Fortnite <br />
+                  <span>Sandbox</span>
+                </h4>
+                <ul>
+                  <li>
+                    <span>4.8</span>
+                  </li>
+                  <li>
+                    <span>2.3</span>
+                  </li>
+                </ul>
+              </div>
+              {/* End:  most-popular-item-content*/}
+            </div>
+            {/* End: card-wrapper */}
+          </div>
+          {/* End: most-popular-item */}
         </div>
         {/* End: most-popular-items */}
       </div>
