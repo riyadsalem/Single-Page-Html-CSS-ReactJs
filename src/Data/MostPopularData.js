7a0f1115d62867261e0d5b549a6dbe3a7115dfe3
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-01 08:59:10 +0200

    Completed >> Section/MostPopular, Data/MostPopularData

---
+++ src/Data/MostPopularData.js
@@ -0,0 +1,41 @@
+import popular_01 from "../assets/images/popular-01.jpg";
+import popular_02 from "../assets/images/popular-02.jpg";
+import popular_03 from "../assets/images/popular-03.jpg";
+import popular_04 from "../assets/images/popular-04.jpg";
+
+const MostPopularData = [
+  {
+    id: "most_popular_item_0",
+    image: popular_01,
+    title: "Frontair",
+    category: "Sandbox",
+    rate: "3.8",
+    download: "1.3M",
+  },
+  {
+    id: "most_popular_item_1",
+    image: popular_02,
+    title: "PubG",
+    category: "Stream-X",
+    rate: "4.8",
+    download: "1.1M",
+  },
+  {
+    id: "most_popular_item_2",
+    image: popular_03,
+    title: "Dota2",
+    category: "Legendary",
+    rate: "2.7",
+    download: "5.3M",
+  },
+  {
+    id: "most_popular_item_3",
+    image: popular_04,
+    title: "GS-Go",
+    category: "Battle S",
+    rate: "3.9",
+    download: "6.1M",
+  },
+];
+
+export default MostPopularData;
--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,46 +1,26 @@
 import "./MostPopular.css";
 import { Card, SectionHeader, SectionWrapper } from "../../components/index";
-import popular_01 from "../../assets/images/popular-01.jpg";
-import popular_02 from "../../assets/images/popular-02.jpg";
-import popular_03 from "../../assets/images/popular-03.jpg";
-import popular_04 from "../../assets/images/popular-04.jpg";
+import MostPopularData from "../../Data/MostPopularData";
 
 const MostPopular = () => {
+  const cards = MostPopularData.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        image={card.image}
+        title={card.title}
+        category={card.category}
+        rate={card.rate}
+        download={card.download}
+      />
+    );
+  });
+
   return (
     <>
       <SectionWrapper>
         <SectionHeader>Most Popular</SectionHeader>
-        {/* End: section-header */}
-        <div className="most-popular-items">
-          <Card
-            image={popular_01}
-            title="Fortnite"
-            category="Sandbox"
-            rate="3.8"
-            download="2.3M"
-          />
-          <Card
-            image={popular_02}
-            title="PubG"
-            category="Stream-X"
-            rate="4.8"
-            download="1.1M"
-          />
-          <Card
-            image={popular_03}
-            title="Dota2"
-            category="Legendary"
-            rate="2.7"
-            download="5.3M"
-          />
-          <Card
-            image={popular_04}
-            title="GS-Go"
-            category="Battle S"
-            rate="3.9"
-            download="6.1M"
-          />
-        </div>
+        <div className="most-popular-items">{cards}</div>
         {/* End: most-popular-items */}
       </SectionWrapper>
     </>
